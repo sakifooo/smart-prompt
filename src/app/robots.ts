51f0e1074c65e45bac41d4289135eb94d3fb13f5
sakifooo <saki.sasin@gmail.com>
2026-06-16 20:18:01 +0100

    Fix sitemap URL in robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://https://ourspase.com/sitemap.xml',
+    sitemap: 'https://ourspase.com/sitemap.xml',
   }
 }
