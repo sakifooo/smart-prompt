fa944fca460bfcfa7279e52bafd9b852ad6b267e
sakifooo <saki.sasin@gmail.com>
2026-07-15 14:21:31 +0100

    Update sitemap URL in robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://ourspase.com/sitemap.xml',
+    sitemap: 'https://www.chardhama.com/sitemap.xml',
   }
 }
