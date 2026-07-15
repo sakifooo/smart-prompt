3dd36ac5c935fc348722a80bc69ae0ebcba09bd1
sakifooo <saki.sasin@gmail.com>
2026-07-15 14:25:25 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,7 +78,7 @@ export default function RootLayout({
           href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='%230ea5e9' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpath d='M12 5a3 3 0 1 0-5.997.125 4 4 0 0 0-2.526 5.77 4 4 0 0 0 .52 5.886 3 3 0 1 0 5.174 2.688 3 3 0 1 0 5.658 0 3 3 0 1 0 5.174-2.688 4 4 0 0 0 .52-5.886 4 4 0 0 0-2.526-5.77A3 3 0 1 0 12 5z'%3E%3C/path%3E%3Cpath d='M9 13a4.5 4.5 0 0 0 3-4'%3E%3C/path%3E%3Cpath d='M15 13a4.5 4.5 0 0 1-3-4'%3E%3C/path%3E%3Cpath d='M12 13V8'%3E%3C/path%3E%3C/svg%3E"
         />
       </head>
-<meta name="google-site-verification" content="2Zhv7aVnqJC6yceL8HJy8eqcDWqIo20cIYvF5FboUX0" />
+<meta name="google-site-verification" content="uq4OvOWYu8KTvby9u_C8Bg0bWB-YWA30Zr4mT1mB7jY" />
       <body className="font-body antialiased bg-white text-slate-900 selection:bg-sky-100">
         {children}
       </body>
