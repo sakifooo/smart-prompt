36ac5973d8126d26e35595a84d8c92a854438717
sakifooo <saki.sasin@gmail.com>
2026-07-15 14:21:49 +0100

    Update sitemap URL from ourspase to chardhama

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://ourspase.com',
+      url: 'https://www.chardhama.com',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1,
